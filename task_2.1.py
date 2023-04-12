82ba8e782dd8f3c2d7bfed22b67e1101b741652b
aidmurataev <muratay@mail.ru>
2023-04-12 23:02:11 +0500

    задача 2.1

---
+++ task_2.1.py
@@ -0,0 +1,10 @@
+def maximum(s): return max(s)
+def minimum(s): return min(s)
+s=[4, 6, 2, 1, 9, 63, -134, 566]
+print("Max=", maximum(s), "Min=", minimum(s))
+s=[-52, 56, 30, 29, -54, 0, -110]
+print("Max=", maximum(s), "Min=", minimum(s))
+s=[42, 54, 65, 87, 0]
+print("Max=", maximum(s), "Min=", minimum(s))
+s=[5]
+print("Max=", maximum(s), "Min=", minimum(s))
